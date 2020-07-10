e721fcd1bbca046162bda1fbfdfa56e1d10e4b01
Mahmud Salomov <mahmud.salomov.salomov@gmail.com>
2020-07-10 22:51:12 +0100

    Yangilash

--- src/main/java/maths/pure/arithmetic/number_feature/Main.java
+++ src/main/java/maths/pure/arithmetic/number_feature/Main.java
@@ -3,6 +3,6 @@ package maths.pure.arithmetic.number_feature;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Number_Feature_Engine.isNatural(0d));
-        System.out.println(Number_Feature_Engine.primeFactors(1024));
+        System.out.println(Number_Feature_Engine.primeFactors(387420489));
     }
 }
