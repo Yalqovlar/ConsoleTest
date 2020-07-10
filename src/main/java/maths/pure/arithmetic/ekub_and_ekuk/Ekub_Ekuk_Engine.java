f73461dcffd0b6daab8e2c82ab87aae9d0017885
Jonibek <ashirov.uzbek.1997@gmail.com>
2020-07-10 22:37:55 +0500

    ekub va ekuk funksiyalari ikkita son uchun miltiekub va multiekuk funksiyalari esa undan ortiq sonlar uchun ishlaydi

--- src/main/java/maths/pure/arithmetic/ekub_and_ekuk/Ekub_Ekuk_Engine.java
+++ src/main/java/maths/pure/arithmetic/ekub_and_ekuk/Ekub_Ekuk_Engine.java
@@ -1,4 +1,46 @@
 package maths.pure.arithmetic.ekub_and_ekuk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Ekub_Ekuk_Engine {
+ public int ekub(int a, int b){
+  while (a != b)
+  {
+   if (a > b) a %= b;
+   else b %= a;
+
+   if (a == 0) a = b;
+   if (b == 0) b = a;
+  }
+  return a;
+ }
+ public int ekuk(int a, int b){
+ return a*b/ekub(a,b);
+
+ }
+ public int multiEkub(List<Integer> list){
+  int a = 0;
+  while (list.size()>1) {
+   List<Integer> list1=new ArrayList<>();
+   for (int i = 1; i < list.size(); i++) {
+    list1.add(ekub(list.get(i - 1), list.get(i)));
+   }
+   list = list1;
+  }
+  a=list.get(0);
+  return a;
+ }
+ public int multiEkuk(List<Integer> list){
+  int a = 0;
+  while (list.size()>1) {
+   List<Integer> list1=new ArrayList<>();
+   for (int i = 1; i < list.size(); i++) {
+    list1.add(ekuk(list.get(i - 1), list.get(i)));
+   }
+   list = list1;
+  }
+  a=list.get(0);
+  return a;
+ }
 }
