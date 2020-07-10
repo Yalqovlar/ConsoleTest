9586cc94991e52a0a44c884dfe8909fa2418667f
Jonibek <ashirov.uzbek.1997@gmail.com>
2020-07-11 00:38:41 +0500

    --//--

--- src/main/java/maths/pure/arithmetic/ekub_and_ekuk/Main.java
+++ src/main/java/maths/pure/arithmetic/ekub_and_ekuk/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
                 List<Integer> list=new ArrayList<>();
                 list.add(20);
                 list.add(30);
